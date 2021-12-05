009de1ff3a74a8a441c802e861ede0265feae7f5
Jimmie Jonasson <jimmie.me.jonasson@gmail.com>
2021-12-05 12:47:34 +0100

    Byter ut react-router-dom till 5.2 för att få withRouter att funka. uppdaterar menu-item för att navigera med withRouter...

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)} // Den matchar vilken url den är på sedan appendar den bara linkUrl:en, framtidssäkrar för eventuella flyttningar av kod.
+  >
     <div
       className="background-image"
       style={{
@@ -16,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
