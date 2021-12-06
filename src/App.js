9a02d9653331e2d0677e571f5becdde268b55570
Jimmie Jonasson <jimmie.me.jonasson@gmail.com>
2021-12-06 21:42:41 +0100

    Adds Header component, with logo and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -8,6 +9,7 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
